c13a73f50c7c32ca95b90d45e5f3f534b1b0f353
Pierre Jacquin <pierre.jacquin@mindmatcher.org>
2023-10-17 17:33:52 +0200

    fsdf

--- scripts/graphQL_Profils.py
+++ scripts/graphQL_Profils.py
@@ -3,52 +3,55 @@ import json
 import streamlit as st
 @st.cache_data(ttl=3600*24)
 def fetch_profil_data():
-    url = st.secrets["graphQL"]
+    # url = st.secrets["graphQL"]
 
-    query = """query User($pagination: paginationInput) {
-                          User(pagination: $pagination) {
-                  id
-                  personalData {
-                    email {
-                      value
-                    }
-                    family {
-                      value
-                    }
-                    given {
-                      value
-                    }
-                  }
-                  experience {
-                    title {
-                      ... on String_xsd {
-                        value
-                      }
-                    }
-                    duration {
-                      value
-                    }
-                  }
-                }
-              }
-    """
-    variables = {
-    "pagination": {
-        "limit": 150
-       }
-    }
-    response = requests.post(
-        url,
-        json={"query": query,"variables":variables}
-    )
+    # query = """query User($pagination: paginationInput) {
+    #                       User(pagination: $pagination) {
+    #               id
+    #               personalData {
+    #                 email {
+    #                   value
+    #                 }
+    #                 family {
+    #                   value
+    #                 }
+    #                 given {
+    #                   value
+    #                 }
+    #               }
+    #               experience {
+    #                 title {
+    #                   ... on String_xsd {
+    #                     value
+    #                   }
+    #                 }
+    #                 duration {
+    #                   value
+    #                 }
+    #               }
+    #             }
+    #           }
+    # """
+    # variables = {
+    # "pagination": {
+    #     "limit": 250
+    #    }
+    # }
+    # response = requests.post(
+    #     url,
+    #     json={"query": query,"variables":variables}
+    # )
 
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print(f"Query failed with status code {response.status_code}")
+    # if response.status_code == 200:
+    #     data = response.json()
+    #     return data
+    # else:
+    #     print(f"Query failed with status code {response.status_code}")
+
+    with open("data.json", "rb") as json_file:
+      data =json.load(json_file)
+    return data
 
 if __name__ == "__main__":
     profiles = fetch_profil_data()
-    print(profiles)
 
